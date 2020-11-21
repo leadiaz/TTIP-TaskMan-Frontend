ee5f4805e8bd9b666f4886f45e54bf735975f02f
gaston da silva <gastonoscardasilva@gmail.com>
2020-11-20 23:43:59 -0300

    se agrego una validacion de email a la hora de registrar un nuevo usuario.

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../models/usuario';
 import { Router } from '@angular/router';
 import { UsuarioService } from '../services/usuario.service';
-import { NgForm } from '@angular/forms';
+import { NgForm,FormBuilder,FormControl,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -11,24 +11,40 @@ import { NgForm } from '@angular/forms';
 export class RegistroComponent implements OnInit {
   isInvalid: boolean = false;
   isError: boolean = false;
-  
-  constructor(private route: Router,private _usuarioService: UsuarioService) { }
-  private user:Usuario = {
-    id: null,
-  	usuario: "",
-  	nombre:"",
-  	apellido:"",
-  	password: "",
-  	email: ""
-  };
+  usuarioForm: FormGroup;
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+  constructor(private route: Router,private _usuarioService: UsuarioService) {
+      this.usuarioForm = this.createFormGroup();
+}
+
+
+  createFormGroup(){
+    return new FormGroup({
+      usuario: new FormControl('',[Validators.required]),
+      nombre: new FormControl('',[Validators.required]),
+      apellido: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required,Validators.minLength(5),Validators.pattern(this.emailPattern)])
+      });
+    }
+
+    onResetForm(){
+        this.usuarioForm.reset();
+       }
 
   ngOnInit() {
   }
   onRegistrar(form: NgForm):void{
-    if(form.valid){
-      this._usuarioService.save(this.user.usuario,this.user.nombre,this.user.apellido,this.user.email,this.user.password)
-      .then(() => this.route.navigate(['login']))
+    if(this.usuarioForm.valid){
+      const user: Usuario = this.usuarioForm.value;
+      this._usuarioService.save(user.usuario,user.nombre,user.apellido,user.email,user.password)
+      .then(() =>{ this.onResetForm();
+                  this.route.navigate(['login']);
+                  })
       .catch(() => {
+
         this.isError = true;
         setTimeout(() => this.isError = false, 4000)});
     }else{
@@ -36,5 +52,21 @@ export class RegistroComponent implements OnInit {
       setTimeout(() => this.isInvalid = false, 4000)
     }
   }
+
+  get usuario(){
+    return this.usuarioForm.get('usuario');
+    }
+    get nombre(){
+      return this.usuarioForm.get('nombre');
+    }
+    get apellido(){
+      return this.usuarioForm.get('apellido');
+    }
+    get password(){
+      return this.usuarioForm.get('password');
+    }
+    get email(){
+      return this.usuarioForm.get('email');
+    }
 
 }
